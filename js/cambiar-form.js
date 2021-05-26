b302b118e91af1bb23ac350ee209fe1be347fe64
cristiangv97 <cristian.a_g.v@hotmail.com>
2021-05-26 18:35:14 -0500

    Se complemento el formulario para el proveedor y se agrego dinamismo a la pagina crear-cuenta.html

--- js/cambiar-form.js
+++ js/cambiar-form.js
@@ -20,7 +20,7 @@ function formularioCliente(){
         "        <input type='text' class='form-control' id='entrada-apellidos' placeholder='Alvarez'>\n" +
         "      </div>\n" +
         "      <div class='mb-3'>\n" +
-        "        <label for='exampleInputEmail1' class='form-label'>Email address</label>\n" +
+        "        <label for='exampleInputEmail1' class='form-label'>Correo electronico</label>\n" +
         "        <input type='email' class='form-control' id='exampleInputEmail1' aria-describedby='emailHelp'>\n" +
         "      </div>\n" +
         "      <div class='mb-3'>\n" +
@@ -32,5 +32,27 @@ function formularioCliente(){
 }
 
 function formularioProveedor(){
-    document.getElementById("formulario-registro").innerHTML = "Completar con formulario de proveedor";
+    document.getElementById("formulario-registro").innerHTML = "\n" +
+        "      <div class='mb-3'>\n" +
+        "        <label for='entrada-nombre' class='form-label'>Nombre de organizacion</label>\n" +
+        "        <input type='text' class='form-control' id='entrada-nombre'>\n" +
+        "      </div>\n" +
+        "      <div class='mb-3'>\n" +
+        "        <label for='entrada-nombre' class='form-label'><em>Campo en blanco</em></label>\n" +
+        "        <input type='text' class='form-control' id='entrada-nombre'>\n" +
+        "      </div>\n" +
+        "      <div class='mb-3'>\n" +
+        "        <label for='entrada-nombre' class='form-label'><em>Campo en blanco</em></label>\n" +
+        "        <input type='text' class='form-control' id='entrada-nombre'>\n" +
+        "      </div>\n" +
+        "      <div class='mb-3'>\n" +
+        "        <label for='exampleInputEmail1' class='form-label'>Correo electronico</label>\n" +
+        "        <input type='email' class='form-control' id='exampleInputEmail1' aria-describedby='emailHelp'>\n" +
+        "      </div>\n" +
+        "      <div class='mb-3'>\n" +
+        "        <label for='entrada-contrasena' class='form-label'>Contraseña</label>\n" +
+        "        <input type='password' class='form-control' id='entrada-contrasena'>\n" +
+        "      </div>\n" +
+        "      <a href='index.html' class='btn btn-primary col-12' role='button'>Registrarse</a>\n" +
+        "      <button type='submit' class='btn btn-primary col-12'>Registrarse <em>(Validaciones)</em></button>";
 }
