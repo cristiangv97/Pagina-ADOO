3217525e89996f16899a156ffb4d85c95b114b72
cristiangv97 <cristian.a_g.v@hotmail.com>
2021-05-27 13:27:32 -0500

    Se agregaron elementos para mostrar la direccion de la sucursal

--- js/mostrar-metodo-pago.js
+++
@@ -1,9 +0,0 @@
-function cambiarTarjeta(){
-    var metodoPago = document.getElementById("num-metodo-pago").value;
-    if (metodoPago === "metodo-1"){
-        document.getElementById("display-metodo-pago").innerHTML = "**** **** **** 5588";
-    }
-    else if (metodoPago === "metodo-2"){
-        document.getElementById("display-metodo-pago").innerHTML = "**** **** **** 6548";
-    }
-}
