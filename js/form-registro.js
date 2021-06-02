4cb357f3c08d193acdd15b9ae65d65ed071be92a
TheHecatombe <72943180+TheHecatombe@users.noreply.github.com>
2021-06-01 21:22:52 -0500

    Agregación de campos en registros.

--- js/form-registro.js
+++ js/form-registro.js
@@ -12,61 +12,67 @@ function checarUsuario() {
 function formularioCliente() {
     document.getElementById("formulario-registro").innerHTML = "<div class=\"form-row\">\n" +
         "      <div class=\"mb-3\">\n" +
-        "        <label for=\"validationCustom01\">Nombre/s</label>\n" +
-        "        <input type=\"text\" class=\"form-control\" id=\"validationCustom01\" placeholder=\"Juan\" required>\n" +
-        "        <div class=\"invalid-feedback\">Favor de ingresar su nombre.</div>\n" +
+        "        <label for=\"validationCustom01\">Nombre/s*</label>\n" +
+        "        <input type=\"text\" class=\"form-control\" id=\"validationCustom01\" required>\n" +
         "      </div>\n" +
         "      <div class=\"mb-3\">\n" +
-        "        <label for=\"validationCustom02\">Apellidos</label>\n" +
-        "        <input type=\"text\" class=\"form-control\" id=\"validationCustom02\" placeholder=\"Perez\" required>\n" +
-        "        <div class=\"invalid-feedback\">Favor de ingresar su apellido.</div>\n" +
+        "        <label for=\"validationCustom02\">Apellido paterno*</label>\n" +
+        "        <input type=\"text\" class=\"form-control\" id=\"validationCustom02\" required>\n" +
+        "      </div>\n" +
+        "      <div class=\"mb-3\">\n" +
+        "        <label for=\"validationCustom05\">Apellido materno*</label>\n" +
+        "        <input type=\"text\" class=\"form-control\" id=\"validationCustom02\" required>\n" +
         "      </div>\n" +
         "    <div class=\"form-row\">\n" +
         "      <div class=\"mb-3\">\n" +
-        "        <label for=\"validationCustom04\">Correo electronico</label>\n" +
-        "        <input type=\"email\" class=\"form-control\" id=\"validationCustom04\" placeholder=\"nombre@email.com\" required>\n" +
-        "        <div class=\"invalid-feedback\">Favor de ingresa un correo valido.</div>\n" +
+        "        <label for=\"validationCustom04\">Correo electronico*</label>\n" +
+        "        <input type=\"email\" class=\"form-control\" id=\"validationCustom04\" required>\n" +
         "      </div>\n" +
         "      <div class=\"mb-3\">\n" +
-        "        <label for=\"validationCustom03\">Contraseña</label>\n" +
+        "        <label for=\"validationCustom03\">Contraseña*</label>\n" +
         "        <input type=\"password\" class=\"form-control\" id=\"validationCustom03\" required>\n" +
-        "        <div class=\"invalid-feedback\">Favor de ingresar una contraseña</div>\n" +
+        "      </div>\n" +
+        "      <div class=\"mb-3\">\n" +
+        "        <label for=\"validationCustom03\">Confirmar contraseña*</label>\n" +
+        "        <input type=\"password\" class=\"form-control\" id=\"validationCustom03\" required>\n" +
         "      </div>\n" +
         "    </div>\n" +
         "    </div>\n" +
+        "    <h6>Todos los campos con un asterisco* son obligatorios.</h6> \n" +
         "    <button class='btn btn-primary col-12' type='submit' id='buton-registro'>Registrarse</button>"
 }
 
 function formularioProveedor(){
     document.getElementById("formulario-registro").innerHTML = "\n" +
         "      <div class=\"mb-3\">\n" +
-        "        <label for=\"validationCustom01\">Razón social / Nombre de la empresa</label>\n" +
-        "        <input type=\"text\" class=\"form-control\" id=\"validationCustom01\" placeholder='Carros Mauricio' required>\n" +
+        "        <label for=\"validationCustom01\">Razón social / Nombre de la empresa*</label>\n" +
+        "        <input type=\"text\" class=\"form-control\" id=\"validationCustom01\" required>\n" +
         "        <div class=\"invalid-feedback\">Favor de ingresar su nombre.</div>\n" +
         "      </div>\n" +
         "    <div class=\"form-row\">\n" +
         "      <div class=\"mb-3\">\n" +
-        "        <label for=\"validationCustom04\">Correo electronico</label>\n" +
-        "        <input type=\"email\" class=\"form-control\" id=\"validationCustom04\" placeholder=\"nombre@email.com\" required>\n" +
+        "        <label for=\"validationCustom04\">Correo electronico*</label>\n" +
+        "        <input type=\"email\" class=\"form-control\" id=\"validationCustom04\" required>\n" +
         "        <div class=\"invalid-feedback\">Favor de ingresa un correo valido.</div>\n" +
         "      </div>\n" +
         "      <div class=\"mb-3\">\n" +
-        "        <label for=\"validationCustom01\">Teléfono</label>\n" +
-        "        <input type=\"tel\" class=\"form-control\" id=\"validationCustom01\" placeholder=\"5563218942\" required>\n" +
+        "        <label for=\"validationCustom01\">Teléfono*</label>\n" +
+        "        <input type=\"tel\" class=\"form-control\" id=\"validationCustom01\" required>\n" +
         "        <div class=\"invalid-feedback\">Favor de ingresar un numero de telefono.</div>\n" +
         "      </div>\n" +
         "      <div class=\"mb-3\">\n" +
-        "        <label for=\"validationCustom03\">Contraseña</label>\n" +
+        "        <label for=\"validationCustom03\">Contraseña*</label>\n" +
         "        <input type=\"password\" class=\"form-control\" id=\"validationCustom03\" required>\n" +
         "        <div class=\"invalid-feedback\">Favor de ingresar una contraseña</div>\n" +
         "      </div>\n" +
         "      <div class=\"mb-3\">\n" +
-        "        <label for=\"validationCustom04\">Confirmar contraseña</label>\n" +
+        "        <label for=\"validationCustom04\">Confirmar contraseña*</label>\n" +
         "        <input type=\"password\" class=\"form-control\" id=\"validationCustom04\" required>\n" +
         "        <div class=\"invalid-feedback\">La contraseña no coincide</div>\n" +
         "      </div>\n" +
         "    </div>\n" +
         "    </div>\n" +
+        "    <h6>Todos los campos con un asterisco* son obligatorios.</h6> \n" +
         "    <button class='btn btn-primary col-12' type='submit' id='buton-registro'>Registrarse</button>"
 
 }
