d71e2c4d893d36bfc45abb9b4c6f33488a01f77c
cristiangv97 <cristian.a_g.v@hotmail.com>
2021-06-02 00:28:51 -0500

    Se reescribio codio para mayor entendimiento

--- js/form-registro.js
+++ js/form-registro.js
@@ -25,37 +25,37 @@ function checarUsuario() {
 function formularioCliente() {
 
     document.getElementById("formulario-registro").innerHTML = "\n"+
-        "   <div class=\"form-row\">\n" +
-        "      <div class=\"mb-3 input-field\">\n" +
-        "        <label for=\"nombre_registro\">Nombre/s*</label>\n" +
-        "        <input type=\"text\" class=\"form-control\" id=\"nombre_registro\" data-validetta=\"letters,required\" data-vd-message-required='Debes llenar el campo de Nombre/s'>" +
+        "   <div class='form-row'>\n" +
+        "      <div class='mb-3 input-field'>\n" +
+        "        <label for='nombre_registro'>Nombre/s*</label>\n" +
+        "        <input type='text' class='form-control' id='nombre_registro' data-validetta='letters,required' data-vd-message-required='Debes llenar el campo de Nombre/s'>" +
         "      </div>\n" +
-        "      <div class=\"mb-3 input-field\">\n" +
-        "        <label for=\"apellidos\">Apellido paterno*</label>\n" +
-        "        <input type=\"text\" class=\"form-control\" id=\"apellidos\" data-validetta=\"letters,required\" data-vd-message-required='Debes llenar el campo de Apellido Paterno'>\n" +
+        "      <div class='mb-3 input-field'>\n" +
+        "        <label for='apellidos'>Apellido paterno*</label>\n" +
+        "        <input type='text' class='form-control' id='apellidos' data-validetta='letters,required' data-vd-message-required='Debes llenar el campo de Apellido Paterno'>\n" +
         "      </div>\n" +
-        "      <div class=\"mb-3 input-field\">\n" +
-        "        <label for=\"apellidos\">Apellido materno*</label>\n" +
-        "        <input type=\"text\" class=\"form-control\" id=\"apellidos\" data-validetta=\"letters,required\" data-vd-message-required='Debes llenar el campo de Apellido Materno'>\n" +
+        "      <div class='mb-3 input-field'>\n" +
+        "        <label for='apellidos'>Apellido materno*</label>\n" +
+        "        <input type='text' class='form-control' id='apellidos' data-validetta='letters,required' data-vd-message-required='Debes llenar el campo de Apellido Materno'>\n" +
         "      </div>\n" +
-        "    <div class=\"form-row\">\n" +
-        "      <div class=\"mb-3 input-field\">\n" +
-        "        <label for=\"correo\">Correo electronico*</label>\n" +
-        "        <input type=\"text\" id=\"correo\" class=\"form-control\" id=\"correo\" data-validetta='required,email' data-vd-message-required='Debes llenar el campo de Correo electronico'>\n" +
+        "    <div class='form-row'>\n" +
+        "      <div class='mb-3 input-field'>\n" +
+        "        <label for='correo'>Correo electronico*</label>\n" +
+        "        <input type='text' id='correo' class='form-control' id='correo' data-validetta='required,email' data-vd-message-required='Debes llenar el campo de Correo electronico'>\n" +
         "      </div>\n" +
-        "      <div class=\"mb-3 input-field\">\n" +
-        "        <label for=\"contrasena\">Contraseña*</label>\n" +
-        "        <input type=\"password\" id=\"contrasena\" name=\"contrasena\" class=\"form-control\" data-validetta=\"password,required\" data-vd-message-required='Debes llenar el campo de Contraseña'>\n" +
+        "      <div class='mb-3 input-field'>\n" +
+        "        <label for='contrasena'>Contraseña*</label>\n" +
+        "        <input type='password' id='contrasena' name='contrasena' class='form-control' data-validetta='password,required' data-vd-message-required='Debes llenar el campo de Contraseña'>\n" +
         "      </div>\n" +
-        "      <div class=\"mb-3 input-field\">\n" +
-        "        <label for=\"contrasena\">Confirmar contraseña*</label>\n" +
-        "        <input type=\"password\" id=\"confirmarContrasena\" class=\"form-control\" data-validetta=\"equalTo[contrasena],required\" data-vd-message-required='Debes llenar el campo de Confirmar contraseña'>\n" +
+        "      <div class='mb-3 input-field'>\n" +
+        "        <label for='contrasena'>Confirmar contraseña*</label>\n" +
+        "        <input type='password' id='confirmarContrasena' class='form-control' data-validetta='equalTo[contrasena],required' data-vd-message-required='Debes llenar el campo de Confirmar contraseña'>\n" +
         "      </div>\n" +
         "    </div>\n" +
         "    </div>\n" +
         "    <h6>Todos los campos con un asterisco* son obligatorios.</h6>\n" +
-        "    <div class=\"input-field\">\n" +
-        "        <input type=\"submit\" class=\"btn btn-primary col-12\" style=\"width: 100%;\" value=\"Registrarse\">\n" +
+        "    <div class='input-field'>\n" +
+        "        <input type='submit' class='btn btn-primary col-12' style='width: 100%;' value='Registrarse'>\n" +
         "    </div>"
 }
 
